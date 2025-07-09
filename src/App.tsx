c6123605bdcb036c38baa140f072903b408e4e1a
devdharunprasad <dharunprasaddev2002@gmail.com>
2025-07-09 15:57:28 +0530

    inital commit

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+  
+     <p>Welcome to Eljay hearing center</p>
+    
+  )
+}
+
+export default App
